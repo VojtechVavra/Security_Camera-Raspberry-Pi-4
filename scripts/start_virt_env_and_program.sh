a1db5a35de0c68253c22819c43a8c24611f2e687
Lord Vooja, the Guardian <60519144+VojtechVavra@users.noreply.github.com>
2022-05-07 13:10:27 +0200

    Update start_virt_env_and_program.sh

--- scripts/start_virt_env_and_program.sh
+++ scripts/start_virt_env_and_program.sh
@@ -9,10 +9,11 @@ cd ~/Projects/Python/tflite/object_detection
 #python3 TFLite_detection_cam2.py --resolution 1280x720 --modeldir=coco_ssd_mobilenet_v1
 #python3 TFLite_detection_cam2.py --resolution 1280x720 --modeldir=coco_ssd_mobilenet_v1
 #python3 TFLite_detection_cam3.py --resolution 1280x720 --modeldir=coco_ssd_mobilenet_v1
-
 #python3 TFLite_detection_cam4.py --resolution 1280x720 --modeldir=coco_ssd_mobilenet_v1
-#python3 TFLite_detection_cam4.py --resolution 1920x1080 --modeldir=coco_ssd_mobilenet_v1
-python3 TFLite_detection_cam4.py --resolution 1640x1232 --modeldir=coco_ssd_mobilenet_v1
+#python3 TFLite_detection_cam4.py --resolution 1640x1232 --modeldir=coco_ssd_mobilenet_v1
+
+python3 TFLite_detection_cam5.py --resolution 1920x1080 --modeldir=coco_ssd_mobilenet_v1
+#python3 TFLite_detection_cam5.py --resolution 1920x1080 --modeldir=coco_ssd_mobilenet_v1
 
 
 # stop virtual env
