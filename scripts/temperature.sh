32678e05770ed22ab4cff8582b68266f43b1e66b
Lord Vooja, the Guardian <60519144+VojtechVavra@users.noreply.github.com>
2022-05-07 13:09:57 +0200

    Update temperature.sh

--- scripts/temperature.sh
+++ scripts/temperature.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-# measuring temperature, can be used without 'watch'
+# -------------------------------------------------------
+# Purpose: Display the ARM CPU and GPU  temperature of Raspberry Pi 2/3/4 
+# -------------------------------------------------------
+cpu=$(</sys/class/thermal/thermal_zone0/temp)
+echo "$(date) @ $(hostname)"
+echo "-------------------------------------------"
+echo "GPU - $(/opt/vc/bin/vcgencmd measure_temp)"
+echo "CPU - $((cpu/1000))'C"
 
-watch vcgencmd measure_temp
+# ARM CPU temperature
+
+# Old use
+# measuring temperature, can be used without 'watch'
+# watch vcgencmd measure_temp
