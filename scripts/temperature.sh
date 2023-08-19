3a9adc6e8fca2996769bd9a95ef2e1330020a454
Lord Vooja, the Guardian <60519144+VojtechVavra@users.noreply.github.com>
2023-08-19 11:37:13 +0200

    Update temperature.sh

--- scripts/temperature.sh
+++ scripts/temperature.sh
@@ -6,6 +6,9 @@ cpu=$(</sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
 echo "GPU - $(/opt/vc/bin/vcgencmd measure_temp)"
+# pokud cesta: neni nalezena, tak použít následující:
+# /usr/bin/vcgencmd measure_temp
+# program vcgencmd lze najít následovně: which vcgencmd
 echo "CPU - $((cpu/1000))'C"
 
 # ARM CPU temperature
